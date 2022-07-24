37e0ea105b06338581012c31e427babc1c80d0c1
mBaenaCh <mateo.baena@hotmail.com>
2022-07-24 14:28:11 -0500

    Subiendo el proyecto al remoto

---
+++ src/main/java/com/pruebatecnica/registrosinmobiliarios/RegistrosinmobiliariosApplication.java
@@ -0,0 +1,13 @@
+package com.pruebatecnica.registrosinmobiliarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrosinmobiliariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrosinmobiliariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pruebatecnica/registrosinmobiliarios/RegistrosinmobiliariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebatecnica.registrosinmobiliarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrosinmobiliariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
