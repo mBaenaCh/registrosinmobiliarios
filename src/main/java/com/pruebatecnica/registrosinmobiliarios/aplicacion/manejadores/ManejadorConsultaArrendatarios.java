0dd9229f2fd7bc3b679c045d13a7d4a7a2e8fe11
mBaenaCh <mateo.baena@hotmail.com>
2022-07-24 18:32:07 -0500

    refactor del manejador, redireccion al dominio

--- src/main/java/com/pruebatecnica/registrosinmobiliarios/aplicacion/manejadores/ManejadorConsultaArrendatarios.java
+++ src/main/java/com/pruebatecnica/registrosinmobiliarios/aplicacion/manejadores/ManejadorConsultaArrendatarios.java
@@ -2,14 +2,25 @@ package com.pruebatecnica.registrosinmobiliarios.aplicacion.manejadores;
 
 import com.pruebatecnica.registrosinmobiliarios.dominio.modelo.Persona;
 import com.pruebatecnica.registrosinmobiliarios.dominio.puerto.RepositorioBusqueda;
+import com.pruebatecnica.registrosinmobiliarios.dominio.servicio.ServicioBusquedaArrendatarios;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class ManejadorConsultaArrendatarios {
 
+    private final ServicioBusquedaArrendatarios servicioBusquedaArrendatarios;
+
+    public ManejadorConsultaArrendatarios(ServicioBusquedaArrendatarios servicioBusquedaArrendatarios){
+        this.servicioBusquedaArrendatarios = servicioBusquedaArrendatarios;
+    }
+
+    public Optional<List<Persona>> buscarArrendatarios(String nombrePropiedad){
+        return servicioBusquedaArrendatarios.buscarArrendatarios(nombrePropiedad);
+    }
 
 
 }
